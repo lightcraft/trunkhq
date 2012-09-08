7fbbf33062e7a247ef8dee2ddb73f6dca3db091a
Ruslan Voloshin <rebisall@gmail.com>
2012-09-08 10:35:27 +0300

    formetted alert page, added colorize for line expired [time: 0.8]

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,9 +1,11 @@
 module ChannelsHelper
-  #def _channel_form_path(channel)
-  #  channel.new_recor
-  #  [current_user.locations.first, @channel ||= Channel.new]
-  #
-  #end
+  def channel_row_class(channel)
+    expired_days = (channel.stop_date - Date.today).to_i
+    return '#f2dede' if expired_days < 0
+    return '#b94a48' if expired_days.eql?(0)
+    return '#3a87ad' if expired_days.eql?(1)
+
+  end
 
   def edit_dom_id(obj)
     dom_id(obj, :edit)
