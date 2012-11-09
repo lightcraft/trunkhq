1662b9e32b9836c37012986f1848fb6b4b26004d
Ruslan Voloshin <rebisall@gmail.com>
2012-11-09 23:10:59 +0200

    updated production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Trunkhq::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application.js  application.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -61,7 +61,7 @@ Trunkhq::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => '85.114.141.80' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
