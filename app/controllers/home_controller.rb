d3fdf6fd62bc2fad5b50b37dfe2caddc3de68bc6
Ruslan Voloshin <rebisall@gmail.com>
2012-12-05 20:01:26 +0200

    fixed provider stats page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,18 +10,20 @@ class HomeController < ApplicationController
     @from = Date.parse(params[:from_date]) rescue Date.today.at_beginning_of_month()
     @to = Date.parse(params[:to_date]) rescue Date.today
 
+    # switch user if have access and provider ID present
+    @user = (current_user.has_admin? && !params[:id].blank?) ?
+        Provider.find_by_id(params[:id]) :
+        current_user
+
+    group_price = @user.has_provider? ? PrefixGroup : PrefixGroupsForProvider
+
     @prefix_groups = {}
     @prefix_rates = {}
 
-    PrefixGroup.all.each do |prefix|
+    group_price.all.each do |prefix|
       @prefix_groups.merge!({prefix.id => prefix.group_name})
       @prefix_rates.merge!({prefix.id => prefix.def_rate})
     end
-
-   # switch user if have access and provider ID present
-    @user = (current_user.has_admin? && !params[:id].blank?) ?
-      Provider.find_by_id(params[:id]) :
-      current_user
 
     @groups_bill = @user.report(@from, @to)
   end
