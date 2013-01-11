cad8fdfd54cb9be918de1001961cace36bcba400
Ruslan Voloshin <rebisall@gmail.com>
2013-01-11 10:03:30 +0200

    return back  stop autoupdate when window is not active

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -5,23 +5,23 @@ $(function () {
     var tab = el;
     var id = $(tab.target).attr('href');
     var path = id.replace(/[\_#]/gi, '/');
-    //  if (window.isActive != false) { //check if window is active and update channel states
-    $(id).load(path, function () {
-      // clean mouse over old tolltips
-      $('.tooltip').hide();
+    if (window.isActive != false) { //check if window is active and update channel states
+      $(id).load(path, function () {
+        // clean mouse over old tolltips
+        $('.tooltip').hide();
 
-      if (location_updater != null) {
-        clearTimeout(location_updater);
-      }
-      location_updater = setTimeout(function () {
-        tab_updater(tab);
-      }, 5000);   // 20 second
+        if (location_updater != null) {
+          clearTimeout(location_updater);
+        }
+        location_updater = setTimeout(function () {
+          tab_updater(tab);
+        }, 20000);   // 20 second
 
-      $('[rel="tooltip"]').tooltip();
-    });
-    //   }else{
-    //     $(id).html('Monitoring stopped!');
-    //   }
+        $('[rel="tooltip"]').tooltip();
+      });
+    } else {
+      $(id).html('Monitoring stopped! Please refresh page if you want see activity.');
+    }
   }
 
   $('#locations a[data-toggle="tab"]').on('shown', function (e) {
