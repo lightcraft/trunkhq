b73f9dd7c50d9a8256efc78f2f5eeaa64c43a037
Ruslan Voloshin <rebisall@gmail.com>
2013-01-13 21:11:25 +0200

    added aditional fileds to prefix grop [time: 2.5]

--- app/models/prefix_group.rb
+++ app/models/prefix_group.rb
@@ -16,7 +16,7 @@ class PrefixGroup < ActiveRecord::Base
   has_many :channels, :through => :chan_prefix_groups
   has_many :cdrs
 
-  attr_accessible :group_name, :def_init_charge, :def_minutes_per_day, :def_rate, :color
+  attr_accessible :group_name, :def_init_charge, :def_minutes_per_day, :def_rate, :color, :member_book_size, :member_age_hours
 
   validates :group_name, :uniqueness => true, :allow_blank => false
   validates :def_rate, :def_init_charge, :def_minutes_per_day, :numericality => true
