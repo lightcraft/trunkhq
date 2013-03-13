81357469fe14c48c12a84e6769f5372fcf79ed1a
Ruslan Voloshin <rebisall@gmail.com>
2013-03-13 12:55:08 +0200

    updated filter [time: 2.3]

--- app/controllers/friend_groups_controller.rb
+++ app/controllers/friend_groups_controller.rb
@@ -80,7 +80,7 @@ class FriendGroupsController < ApplicationController
 
   def channels_assignment
     @channels = current_user.channels
-    @channels = @channels.where(:location_id => params[:location_id] ) if params[:location_id]
+    @channels = @channels.where(:friend_group_id => params[:friend_group_id] ) if params[:location_id]
     @friend_groups = current_user.friend_groups
   end
 
